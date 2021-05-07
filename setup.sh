265b597f34b0926f20b791b462a409e73d405ebf
Reid Ransom <reid@reidransom.com>
2021-05-07 13:13:21 -0400

    bump docker-compose

--- setup.sh
+++ setup.sh
@@ -43,6 +43,6 @@ sudo usermod -aG docker $USER
 sudo systemctl enable docker
 
 # Use latest release number from [releases page](https://github.com/docker/compose/releases).
-sudo curl -L "https://github.com/docker/compose/releases/download/1.25.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 
 sudo chmod +x /usr/local/bin/docker-compose
